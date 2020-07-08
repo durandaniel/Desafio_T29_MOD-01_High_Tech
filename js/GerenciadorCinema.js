6d8a90c889efbbfc3ea8e1dd1efd0f68ad8642be
Daniel Aredes Vendramini Duran <vendraminidaniel@gmail.com>
2020-07-08 13:46:45 -0400

    css + paginas cadastro + table

--- js/GerenciadorCinema.js
+++ js/GerenciadorCinema.js
@@ -10,6 +10,47 @@ class GerenciadorCinema {
     constructor() {
         this.sessoes = [];
     }
+
+    inserirLinha() {
+        let tabela = document.getElementById("tabela");
+
+        let linha = tabela.insertRow();
+        linha.id = "linha-" + this.contador;
+
+        let colunaNome = linha.insertCell();
+        let colunaIdade = linha.insertCell();
+        let colunaEmail = linha.insertCell();
+        let colunaExcluir = linha.insertCell();
+        let colunaEditar = linha.insertCell();
+
+        colunaNome.innerText = convidado.nome;
+        colunaIdade.innerText = convidado.idade;
+        colunaEmail.innerText = convidado.email;
+
+        let imgExcluir = document.createElement("img");
+        imgExcluir.src = "img/deletar.svg";
+        imgExcluir.setAttribute(
+            "onclick", `gerenciadorCinema.remover(${this.contador})`
+        );
+
+        colunaExcluir.appendChild(imgExcluir);
+
+        let imgEditar = document.createElement("img");
+        imgEditar.src = "img/editar.svg";
+        imgEditar.setAttribute(
+            "onclick", `gerenciadorCinema.editar(${this.contador})`
+        );
+
+        colunaEditar.appendChild(imgEditar);
+    }
+
+
+
+
+
+
+
+
 }
 
 let gerenciadorCinema = new GerenciadorCinema;
